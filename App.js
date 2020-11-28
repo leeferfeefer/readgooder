5c9e888e2be8735da4a0bf65ae27916a728ff0fc
leeferfeefer <danbfjr@gmail.com>
2020-11-28 16:06:23 -0500

    Added styling and icons to the tabbar

--- App.js
+++ App.js
@@ -3,19 +3,53 @@ import {
   SafeAreaView,
   StyleSheet,
   StatusBar,
-  Text,
-  View
+  View,
+  Text
 } from 'react-native';
 import {version} from './package.json';
 import WordViewer from './screens/WordViewer';
+import WordGame from './screens/WordGame';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
+
+const WordViewerScreen = () => {
+  return <WordViewer/>;
+}
+
+const WordGameScreen = () => {
+  return <WordGame/>;
+};
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={styles.container}>      
-        <WordViewer/>
-        <Text>v{version}</Text>
+      <SafeAreaView style={styles.container}>  
+        <Text style={{textAlign: 'center'}}>v{version}</Text>
+        <NavigationContainer>  
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+                if (route.name === 'WordViewer') {
+                  iconName = focused ? 'md-library' : 'md-library-outline';
+                } else if (route.name === 'WordGame') {
+                  iconName = focused ? 'md-game-controller' : 'md-game-controller-outline';
+                }    
+                return <Ionicons name={iconName} size={size} color={color}/>;
+              },
+            })}
+            tabBarOptions={{
+              activeTintColor: 'tomato',
+              inactiveTintColor: 'gray',
+            }}>
+            <Tab.Screen name="Words" component={WordViewerScreen}/>
+            <Tab.Screen name="Game" component={WordGameScreen}/>
+          </Tab.Navigator>
+        </NavigationContainer>   
       </SafeAreaView>
     </>
   );
@@ -24,7 +58,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
+    marginTop: StatusBar.currentHeight || 0
   }
 });
 
---
+++ screens/WordGame.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+const WordGame = () => {
+    return (
+        <View>
+            <Text>Word Game Here</Text>
+        </View>
+    );
+};
+
+export default WordGame;
+
